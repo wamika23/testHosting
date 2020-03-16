d6e66ef37e082774e61d0f61314554c02e85f3e3
sagar\sagar_2621 <sagarb@triconinfotech.com>
2020-03-16 17:22:16 +0530

    Updates

---
+++ precache-manifest.313e867b6e0d593862006c92e0412e73.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec09ec8798e2f8256f5892a4e9cc9e19",
+    "url": "/wamika23.co.uk/index.html"
+  },
+  {
+    "revision": "94d48180979e454b867c",
+    "url": "/wamika23.co.uk/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e0857ad1fd5c3ce4e474",
+    "url": "/wamika23.co.uk/static/js/2.ad1e2baa.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/wamika23.co.uk/static/js/2.ad1e2baa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94d48180979e454b867c",
+    "url": "/wamika23.co.uk/static/js/main.935dc4d1.chunk.js"
+  },
+  {
+    "revision": "9de385d99eb9a9f14134",
+    "url": "/wamika23.co.uk/static/js/runtime-main.2ac281a8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/wamika23.co.uk/static/media/logo.5d5d9eef.svg"
+  }
+]);
