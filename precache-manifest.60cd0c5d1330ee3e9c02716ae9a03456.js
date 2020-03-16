5c9f518147e9cb165caf6351d4df7dabc8e3c7fc
sagar\sagar_2621 <sagarb@triconinfotech.com>
2020-03-16 17:35:20 +0530

    Updates

---
+++ precache-manifest.60cd0c5d1330ee3e9c02716ae9a03456.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d56c00e492dda0799fccc28ba072ad2e",
+    "url": "/testHosting/index.html"
+  },
+  {
+    "revision": "1ba25adf20bd43b7f835",
+    "url": "/testHosting/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fb6f4c1096d7d6100ec4",
+    "url": "/testHosting/static/js/2.22ee7116.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/testHosting/static/js/2.22ee7116.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ba25adf20bd43b7f835",
+    "url": "/testHosting/static/js/main.019c6490.chunk.js"
+  },
+  {
+    "revision": "ac62292b67c57d05f46a",
+    "url": "/testHosting/static/js/runtime-main.cdfb3513.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testHosting/static/media/logo.5d5d9eef.svg"
+  }
+]);
